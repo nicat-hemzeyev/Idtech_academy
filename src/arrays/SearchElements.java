a5ed1c79ce5f5b5f779b359208759ed92e27e560
Nicat Hemzeyev <hemzeyevnicat25@gmail.com>
2026-02-07 22:52:12 +0400

    update

--- src/arrays/SearchElements.java
+++ src/arrays/SearchElements.java
@@ -5,17 +5,18 @@ import java.util.Scanner;
 public class SearchElements {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Bir eded daxil edin:");
+        System.out.println("Enter search number:");
         int eded = sc.nextInt();
         int[] arr = {3, 2, 1, 6, 9, 7};
+        int a = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == eded) {
-                System.out.println("Daxil etdiyiniz eded dogrudur.");
-
-            else System.out.println("Daxil etdiyiniz eded yanlisdir.");
+                System.out.println("true");
+                a++;
             }
-
-
+        }
+        if (a == 0) {
+            System.out.println("false");
         }
     }
 }
